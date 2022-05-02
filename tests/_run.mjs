05be8a9b8bfaef3b35cd5de453d6677f5d8a329e
Hugo Di Francesco <HugoDF@users.noreply.github.com>
2022-05-02 18:51:19 +0100

    test file watching?

--- tests/_run.mjs
+++ tests/_run.mjs
@@ -3,6 +3,10 @@
  */
 import fs from 'node:fs/promises';
 import path from 'node:path';
+import process from 'node:process';
+
+const ARGV = process.argv.slice(2);
+const isWatch = ARGV.includes('-w') || ARGV.includes('--watch');
 
 async function importAllTestFiles() {
   const currentModulePath = new URL(import.meta.url).pathname;
@@ -13,10 +17,19 @@ async function importAllTestFiles() {
     (file) => file !== __filename
   );
 
+
   const importPromises = testFiles.map((file) => {
     return import(path.resolve(__dirname, file));
   });
   await Promise.all(importPromises);
+
+  if (isWatch) {
+    // const watcher = fs.watch(__dirname, { recursive: true });
+    // for await (const event of watcher) {
+    //   console.log(event);
+    //   await import(filePath);
+    // }
+  }
 }
 
 importAllTestFiles();
